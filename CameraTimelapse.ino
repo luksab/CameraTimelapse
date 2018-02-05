f1000e6afbf47cb162fb065f0bf1ea5975c3a60e
luksab <luksab@users.noreply.github.com>
2018-02-05 09:29:52 +0100

    cgfhjk

--- CameraTimelapse.ino
+++ CameraTimelapse.ino
@@ -7,23 +7,21 @@
 #define STEP_PIN2  27                               // Step on rising edge
 
 HardwareSerial Serial2(2);
-
-#include <SPI.h>
-#include "RF24.h"
+#include <WiFi.h>
+const char* ssid     = "Timelapse";
+const char* password = "Password";
+WiFiServer server(80);
 
 #include <TMC2208Stepper.h>                       // Include library
 TMC2208Stepper driver = TMC2208Stepper(&Serial2); // Create driver and use
 // HardwareSerial0 for communication
 
-RF24 radio(21, 22);
-byte addresses[][6] = {"1Node", "2Node"};
-
 unsigned long start1;
 unsigned long start2;
 long zeitU = 10000000.0;
+int Steps = 10;
 int delay1 =  100;
 int delay2 = 1600;
-bool start = false;
 long startPosY = 0;
 long startPosX = 0;
 long endPosY = 0;
@@ -40,14 +38,15 @@ void setup() {
 
   Serial.println("Hi!");
 
-  radio.begin();
-  radio.setPALevel(RF24_PA_LOW); // LOW power
-  radio.openWritingPipe(addresses[0]);
-  radio.openReadingPipe(1, addresses[1]);
-  radio.startListening();
-
   Serial2.begin(115200);                            // Init used serial port
   while (!Serial2);
+
+  WiFi.softAP(ssid, password);
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.softAPIP());
 
   // Prepare pins
   pinMode(DIR_PIN1, OUTPUT);
@@ -67,75 +66,144 @@ void setup() {
   digitalWrite(DIR_PIN2, LOW);
   start1 = micros();
   start2 = micros();
+  server.begin();
 }
 
 void loop() {
-  if ( radio.available()) {
-    int len = 0;
-    len = radio.getDynamicPayloadSize();
-    char cha[len] = "";
-    radio.read( &cha, len );
-
-    radio.stopListening();
-    radio.write( &cha, strlen(cha) ); //send back controll
-    radio.startListening();
-    if (strlen(cha) > 0) {
-      char** args = str_split(cha, ',');
-      //for (int i=0;i<5;i++)
-      //  Serial.println(args[i]);
-      if (strcmp(args[0], "startPos") == 0) {
-        startPosX = posX;
-        startPosY = posY;
-      }
-      else if (strcmp(args[0], "endPos") == 0) {
-        endPosX = posX;
-        endPosY = posY;
-      }
-      else if (strcmp(args[0], "start") == 0) {
-        start = true;
-        if ((abs(posY - endPosY) != 0) && (abs(posX - endPosX) != 0)) {
-          delay1 = (int)(zeitU / (abs(posY - endPosY)));
-          delay2 = (int)(zeitU / (abs(posX - endPosX)));
+  WiFiClient client = server.available();
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        //Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println();
+            client.println("<!DOCTYPE HTML><html><head>");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
+            client.println("<body><h1>Timelapse WiFi Remote</h1>");
+            client.println("<p>X-Achse <a href=\"up\"><button>Up</button></a>&nbsp;<a href=\"down\"><button>Down</button></a></p>");
+            client.println("<p>Y-Achse <a href=\"left\"><button>Left</button></a>&nbsp;<a href=\"right\"><button>Right</button></a></p>");
+            client.println("<p>Set <a href=\"StartPos\"><button>StartPos</button></a>&nbsp;<a href=\"EndPos\"><button>EndPos</button></a> to current Position</p>");
+            client.print("<a href=\"/goToStart\">Go to the Startposition</a><br>");
+            client.print("<a href=\"/goToEnd\">Go to the Endposition</a><br>");
+            client.print("<form><table>");
+            client.print("<tr><th>Time in s</th><th><input name=\"Time\" value=\"");client.print(zeitU/1000000);client.print("\" /></th></tr>");
+            client.print("<tr><th>Steps per Click</th><th><input name=\"Steps\" value=\"");client.print(Steps);client.print("\" /></th></tr>");
+            client.print("</table><br/><input type=\"submit\" value=\"Set\" />");
+            client.println("</form><br>");
+            client.println("<a href=\"start\"><button>Start</button></a>");
+            client.println("<a href=\"reset\"><button>Reset</button></a>");
+            client.println("<a href=\"ResetSpeed\"><button>Reset Speed</button></a>");
+            client.println("</body></html>");
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine And PROCESS STUFF!!!
+            if (currentLine.indexOf("ET") > 0 && currentLine.indexOf("/favicon.ico") <= 0) {
+              Serial.println("LINE!!!");
+              if (currentLine.indexOf("Time") > 0) {
+                Serial.println(currentLine.substring(currentLine.indexOf("Time") + 5));
+                String dataFileName = currentLine.substring(currentLine.indexOf("Time") + 5, currentLine.indexOf("&"));//from the end of Time to the next space
+                char __dataFileName[sizeof(dataFileName)];
+                dataFileName.toCharArray(__dataFileName, sizeof(__dataFileName));
+                Serial.println(dataFileName);
+                zeitU = atoi(__dataFileName) * 1000000;
+              }
+              if (currentLine.indexOf("Steps") > 0) {
+                Serial.println("-------------");
+                Serial.println(currentLine.substring(currentLine.indexOf("Steps") + 6));
+                String dataFileName = currentLine.substring(currentLine.indexOf("Steps") + 6,
+                                      currentLine.length()-8);//from the end of Time to the next space
+                char __dataFileName[sizeof(dataFileName)];
+                dataFileName.toCharArray(__dataFileName, sizeof(__dataFileName));
+                Serial.println(dataFileName);
+                Steps = atoi(__dataFileName);
+              }
+              else if (currentLine.indexOf("up") > 0)
+                goalPosY += Steps * 16;
+              else if (currentLine.indexOf("down") > 0 && goalPosY - 10 > 0)
+                goalPosY -= Steps * 16;
+              else if (currentLine.indexOf("left") > 0)
+                goalPosX -= Steps;
+              else if (currentLine.indexOf("right") > 0)
+                goalPosX += Steps;
+              else if (currentLine.indexOf("start") > 0) {
+                if ((abs(posY - endPosY) != 0) && (abs(posX - endPosX) != 0)) {
+                  delay1 = (int)(zeitU / (abs(posY - endPosY)));
+                  delay2 = (int)(zeitU / (abs(posX - endPosX)));
+                }
+                Serial.println(delay1 + " " + delay2);
+                goalPosX = endPosX;
+                goalPosY = endPosY;
+              }
+              else if (currentLine.indexOf("goToStart") > 0) {
+                goalPosX = startPosX;
+                goalPosY = startPosY;
+              }
+              else if (currentLine.indexOf("goToEnd") > 0) {
+                goalPosX = endPosX;
+                goalPosY = endPosY;
+              }
+              else if (currentLine.indexOf("StartPos") > 0) {
+                startPosX = posX;
+                startPosY = posY;
+              }
+              else if (currentLine.indexOf("EndPos") > 0) {
+                endPosX = posX;
+                endPosY = posY;
+              }
+              else if (currentLine.indexOf("ResetSpeed") > 0) {
+                driver.pdn_disable(1);                          // Use PDN/UART pin for communication
+                driver.I_scale_analog(0);                       // Adjust current from the registers
+                driver.rms_current(500);                        // Set driver current 500mA
+                driver.toff(0x2);                               // Enable driver
+                driver.microsteps(256);
+                driver.mstep_reg_select(1);
+                driver.en_spreadCycle(false);
+                delay1 =  100;
+                delay2 = 1600;
+              }
+              else if (currentLine.indexOf("reset") > 0) {
+                driver.pdn_disable(1);                          // Use PDN/UART pin for communication
+                driver.I_scale_analog(0);                       // Adjust current from the registers
+                driver.rms_current(500);                        // Set driver current 500mA
+                driver.toff(0x2);                               // Enable driver
+                driver.microsteps(256);
+                driver.mstep_reg_select(1);
+                driver.en_spreadCycle(false);
+                delay1 =  100;
+                delay2 = 1600;
+                startPosY = 0;
+                startPosX = 0;
+                endPosY = 0;
+                endPosX = 0;
+                goalPosY = 0;
+                goalPosX = 0;
+              }
+            }
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
         }
-        Serial.println(delay1 + " " + delay2);
-        goalPosX = endPosX;
-        goalPosY = endPosY;
-      }
-      else if (strcmp(args[0], "goToStart") == 0) {
-        start = true;
-        goalPosX = startPosX;
-        goalPosY = startPosY;
-      }
-      else if (strcmp(args[0], "setTime") == 0) {
-        zeitU = atoi(args[1]) * 1000000;
-      }
-      else if (strcmp(args[0], "restart") == 0) {
-        driver.pdn_disable(1);                          // Use PDN/UART pin for communication
-        driver.I_scale_analog(0);                       // Adjust current from the registers
-        driver.rms_current(500);                        // Set driver current 500mA
-        driver.toff(0x2);                               // Enable driver
-        driver.microsteps(256);
-        driver.mstep_reg_select(1);
-        driver.en_spreadCycle(false);
-        delay1 =  100;
-        delay2 = 1600;
-        start = false;
-        startPosY = 0;
-        startPosX = 0;
-        endPosY = 0;
-        endPosX = 0;
-        goalPosY = 0;
-        goalPosX = 0;
-      }
-      else {
-        //delay1 = atoi(args[0]);
-        //delay2 = atoi(args[1]);
-        goalPosX += atoi(args[0]);
-        if (goalPosY + atoi(args[1]) > 0)
-          goalPosY += atoi(args[1]);
       }
     }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
   }
+
+
   if (!(micros() - start1 < delay1)) {
     start1 = micros();
     if (goalPosY > posY) {
